b58959626e487182203704f8ed8aa808de383e16
felipefoschiera <felipefoschiera@gmail.com>
2022-07-04 12:00:37 -0300

    Updates

---
+++ precache-manifest.20a48ee556e5df794b2f4d7f52fd947f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4048fcc0ba5cfae76964",
+    "url": "/stardew-gift-helper/static/css/main.51a5b888.chunk.css"
+  },
+  {
+    "revision": "4048fcc0ba5cfae76964",
+    "url": "/stardew-gift-helper/static/js/main.4048fcc0.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/stardew-gift-helper/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/stardew-gift-helper/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "b2db2cd80ab9cc5db809",
+    "url": "/stardew-gift-helper/static/js/runtime~main.b2db2cd8.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/stardew-gift-helper/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "cf8da71177c620a96c5cecd547d0eb51",
+    "url": "/stardew-gift-helper/index.html"
+  }
+];
