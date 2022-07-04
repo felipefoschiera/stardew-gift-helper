dd76db7a63848159b65a38f402fe8d73c2f5f600
felipefoschiera <felipefoschiera@gmail.com>
2022-07-04 14:06:57 -0300

    Set up Prettier and run

--- stardew-gift-helper/src/App.js
+++ stardew-gift-helper/src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
 export class App extends Component {
-
   render() {
     return (
       <div className="App">
